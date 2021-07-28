856faf4e327bbbbb08b9e086a211cd8b8ffa393b
wangzekun <wangzekun@bonc.com.cn>
2021-07-28 11:38:04 +0800

    提交项目

---
+++ src/main/java/com/potucs/ProductController.java
@@ -0,0 +1,55 @@
+package com.potucs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther wangzekun
+ * @Date 2021/7/28 11:22
+ * @Description
+ */
+@Controller
+@RequestMapping("/product")
+public class ProductController{
+
+    /**
+     * 商品首页
+     */
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    /**
+     * 商品添加
+     */
+    @RequestMapping("/add")
+    public String add(){
+        return "product/productAdd";
+    }
+
+    /**
+     * 商品修改
+     */
+    @RequestMapping("/update")
+    public String update(){
+        return "product/productUpdate";
+    }
+
+    /**
+     * 商品列表
+     */
+    @RequestMapping("/list")
+    public String list(){
+        return "product/productList";
+    }
+
+    /**
+     * 商品删除
+     */
+    @RequestMapping("/delete")
+    public String delete(){
+        return "product/productDelete";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>首页</title>
+</head>
+<body>
+以下是网站的功能：<br/>
+<a href="${pageContext.request.contextPath}/product/add">商品添加</a><br/>
+<a href="${pageContext.request.contextPath}/product/update">商品修改</a><br/>
+<a href="${pageContext.request.contextPath}/product/list">商品列表</a><br/>
+<a href="${pageContext.request.contextPath}/product/delete">商品删除</a><br/>
+</body>
+</html>
